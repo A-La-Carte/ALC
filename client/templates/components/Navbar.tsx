878196899a9f058943f8385b062fb428e765c4bd
Rohan Varma <rohan@prompthire.com>
2019-09-28 14:49:00 -0700

    Add background image + navbar stuff

--- client/templates/components/Navbar.tsx
+++ client/templates/components/Navbar.tsx
@@ -9,11 +9,11 @@ export default class Navbar extends React.PureComponent<Props, State> {
     return (
       <div className="alc-navbar">
         <div className="alc-navbar-left">
-          <div className="alc-navbar-main">A La Carte</div>
+          <div className="alc-navbar-main"><div>A La Carte</div></div>
         </div>
         <div className="alc-navbar-right">
-          <div className="alc-navbar-link">Restaurants</div>
-          <div className="alc-navbar-link">Recipes</div>
+          <div className="alc-navbar-link"><div>Restaurants</div></div>
+          <div className="alc-navbar-link"><div>Recipes</div></div>
         </div>
       </div>
     );
