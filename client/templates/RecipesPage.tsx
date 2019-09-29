b8bf590e27d64ac12566dc6b2194ef5dcf0a4c0e
Rohan Varma <rohan@prompthire.com>
2019-09-29 12:42:28 -0700

    Add 4 fake recipes data + make the recipes page load dynamically based on it

--- client/templates/RecipesPage.tsx
+++ client/templates/RecipesPage.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import _ from 'underscore';
 import PageContainer from './components/PageContainer'
 import SearchResult from './components/SearchResult'
+import recipesData from '../../data/recipes.json';
+import restaurantData from '../../data/restaurants.json'
 
+const restaurantLookup = _.indexBy(restaurantData, 'id')
 
 interface Props {
 }
@@ -12,22 +16,13 @@ export default class RecipesPage extends React.PureComponent<Props, State> {
     return (
       <PageContainer>
         <div className="search-results-section row">
-          <SearchResult
-            restaurant="Bestia"
-            title="Assa Bucco"
-            img="https://bestiala.com/wp-content/uploads/sites/4/2017/01/slider1.jpg" />
-          <SearchResult
-            restaurant="Momofuku"
-            title="Chicken Tikka Masala"
-            img="https://momofuku-assets.s3.amazonaws.com/uploads/2019/04/majordomo.jpg" />
-          <SearchResult
-            restaurant="Bestia"
-            title="Pasta Bolognese"
-            img="https://bestiala.com/wp-content/uploads/sites/4/2015/07/SMALL-Home-Main-Event-Bottom-Right-.jpg" />
-          <SearchResult
-            restaurant="Momofuku"
-            title="Fried Checkin Burger"
-            img="https://momofuku-assets.s3.amazonaws.com/uploads/2016/11/Spicy-Fried-Chicken-Sandwiches-please-credit-Zack-DeZon.jpg" />
+          {recipesData.map(recipe =>
+            <SearchResult
+              key={recipe.id}
+              restaurant={restaurantLookup[recipe.restaurant_id].name}
+              title={recipe.name}
+              img={recipe.main_image} />
+          )}
         </div>
       </PageContainer>
     );
