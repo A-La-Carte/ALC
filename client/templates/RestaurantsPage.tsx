88d2ba89384b55412b28ebb041890403e71b2b72
Rohan Varma <rohan@prompthire.com>
2019-10-02 08:37:44 -0700

    Add a few real restauarant data + make restaurants list page render dynamically

--- client/templates/RestaurantsPage.tsx
+++ client/templates/RestaurantsPage.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import PageContainer from './components/PageContainer'
 import SearchResult from './components/SearchResult'
+import restaurantData from '../../data/restaurants.json'
 
 
 interface Props {
@@ -12,23 +13,14 @@ export default class RestaurantsPage extends React.PureComponent<Props, State> {
     return (
       <PageContainer selectedRoute="restaurants">
         <div className="search-results-section row">
-          <SearchResult
-            href="/restaurants/1"
-            restaurant="Bestia"
-            title="Assa Bucco"
-            img="https://bestiala.com/wp-content/uploads/sites/4/2017/01/slider1.jpg" />
-          <SearchResult
-            restaurant="Momofuku"
-            title="Chicken Tikka Masala"
-            img="https://momofuku-assets.s3.amazonaws.com/uploads/2019/04/majordomo.jpg" />
-          <SearchResult
-            restaurant="Bestia"
-            title="Pasta Bolognese"
-            img="https://bestiala.com/wp-content/uploads/sites/4/2015/07/SMALL-Home-Main-Event-Bottom-Right-.jpg" />
-          <SearchResult
-            restaurant="Momofuku"
-            title="Fried Checkin Burger"
-            img="https://momofuku-assets.s3.amazonaws.com/uploads/2016/11/Spicy-Fried-Chicken-Sandwiches-please-credit-Zack-DeZon.jpg" />
+          {restaurantData.map(restaurant =>
+            <SearchResult
+              href={`/restaurants/${restaurant.id}`}
+              restaurant={restaurant.address_city_state}
+              title={restaurant.name}
+              img={restaurant.main_image}
+            />
+          )}
         </div>
       </PageContainer>
     );
